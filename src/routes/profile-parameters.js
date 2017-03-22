ce6e5e940c8fe67d06e885944b9ece2f9fcd22ad
Artur Ciocanu <ciocanu@adobe.com>
2017-03-21 20:30:32 -0700

    Ensure XT related examples choose random experience

--- src/routes/profile-parameters.js
+++ src/routes/profile-parameters.js
@@ -1,11 +1,17 @@
 const Component = require("../components/catalog").PROFILE_PARAMETERS;
 const render = require("../services/rendering-service");
 
+function chooseType() {
+  const rand = Math.floor(Math.random() * 1000);
+
+  return rand % 2 === 0 ? "autobot" : "decepticon";
+}
+
 module.exports = (req, res) => {
   const payload = {
     mbox: "a1-serverside-profileparams",
     profileParameters : {
-      "transformerType" : "autobot"
+      "transformerType" : chooseType()
     }
   };
 
--- src/routes/xt.js
+++ src/routes/xt.js
@@ -1,11 +1,22 @@
 const Component = require("../components/catalog").XT;
 const render = require("../services/rendering-service");
 
+function chooseCountry() {
+  const rand = Math.floor(Math.random() * 1000);
+
+  switch (rand % 4) {
+    case 0: return "usa";
+    case 1: return "uk";
+    case 2: return "france";
+    case 3: return "australia";
+  }
+}
+
 module.exports = (req, res) => {
   const payload = {
     mbox: "a1-serverside-xt",
     profileParameters: {
-      country: "usa"
+      country: chooseCountry()
     }
   };
 
